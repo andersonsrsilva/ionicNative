607d770df8251272585ca96b9b1e019250e82853
anderson.silva <andersonsrsilva@gmail.com>
2017-12-07 12:41:52 -0200

    MapStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {Geolocation} from '@ionic-native/geolocation'
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {GoogleMaps} from "@ionic-native/google-maps";
 
 @NgModule({
   declarations: [
@@ -22,9 +23,12 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    GoogleMaps,
+    Geolocation,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,17 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Geolocation, Geoposition} from "@ionic-native/geolocation";
+import {Platform} from "ionic-angular";
+import {
+  CameraPosition,
+  GoogleMap,
+  GoogleMaps,
+  GoogleMapsEvent,
+  ILatLng,
+  LatLng,
+  Marker,
+  MarkerOptions
+} from "@ionic-native/google-maps";
+import {MapStyle} from '../../app/mapStyle';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +19,72 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: GoogleMap
 
+  constructor(public geolocation: Geolocation,
+              public googleMaps: GoogleMaps) {
+  }
+
+  ngAfterViewInit() {
+    let loc: LatLng;
+    this.initMap();
+
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      this.getLocation().then((res) => {
+        loc = new LatLng(res.coords.latitude, res.coords.longitude);
+        this.moveCamera(loc);
+
+        this.createMaker(loc, 'Me')
+          .then((marker: Marker) => {
+            marker.showInfoWindow();
+          }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      })
+    });
+  }
+
+  moveCamera(loc: LatLng) {
+    let options: CameraPosition<ILatLng> = {
+      target: loc,
+      zoom: 15,
+      tilt: 10
+    }
+
+    this.map.moveCamera(options);
+  }
+
+  initMap() {
+    let style = [];
+    let element = this.mapElement.nativeElement;
+
+    if (this.isNight()) {
+      style = MapStyle;
+    }
+
+    this.map = this.googleMaps.create(element, {styles: style});
+  }
+
+  getLocation() {
+    return this.geolocation.getCurrentPosition();
+  }
+
+  createMaker(loc: LatLng, title: string) {
+    let markerOptions: MarkerOptions = {
+      position: loc,
+      title: title
+    };
+
+    return this.map.addMarker(markerOptions);
+  }
+
+  isNight() {
+    let time = new Date().getHours();
+
+    return (time > 5 && time < 19) ? true : false;
   }
 
 }
